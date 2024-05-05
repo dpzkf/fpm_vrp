3c75e55e5957f179fd3725034e5019a94e9f67c7
Ilya <dpzkfzzz@gmail.com>
2024-05-06 02:44:33 +0300

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
-  base: "/<fpm_vrp>/",
+  base: "/fpm_vrp/",
   server: {
     port: 3000,
   },
