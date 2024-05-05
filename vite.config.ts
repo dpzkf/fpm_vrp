c7468a07a5a53f0f426deb44054f9bc14682c5c0
Ilya <dpzkfzzz@gmail.com>
2024-05-06 02:35:35 +0300

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
-  base: "/vite-deploy/",
   server: {
     port: 3000,
   },
