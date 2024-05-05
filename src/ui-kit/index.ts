e01d6414e3c2241382ed8dab4017289696451a67
Ilya <dpzkfzzz@gmail.com>
2024-05-06 02:25:10 +0300

    fix: exports

--- src/ui-kit/index.ts
+++ src/ui-kit/index.ts
@@ -1,41 +1,18 @@
-import { Logo } from "./common/Logo";
-import { SvgIcon } from "./common/SvgIcon";
-import { Button } from "./interactive/Button";
-import { Checkbox } from "./interactive/Checkbox";
-import { Input } from "./interactive/Input";
-import { InputPassword } from "./interactive/InputPassword";
-import { InputSearch } from "./interactive/InputSearch";
-import { InputWithVerticalLabel } from "./interactive/InputWithVerticalLabel";
-import { ConfirmModal, DeleteModal } from "./interactive/Modal";
-import { NumberInputWithVerticalLabel } from "./interactive/NumberInputWithVerticalLabel";
-import { RadioGroupWithVerticalLabel } from "./interactive/RadioGroupWithVerticalLabel";
-import { Selectbox } from "./interactive/Selectbox";
-import { SortButton } from "./interactive/SortButton";
-import { Switch } from "./interactive/Switch";
-import { SwitchWithVerticalLabel } from "./interactive/SwitchWithVerticalLabel";
-import { Tabs, TabsContainer } from "./interactive/Tabs";
-import { Textarea } from "./interactive/Textarea";
-import { Text } from "./typography/Text";
-
-export {
-  Input,
-  Button,
-  Checkbox,
-  Text,
-  InputPassword,
-  SvgIcon,
-  Logo,
-  InputWithVerticalLabel,
-  Selectbox,
-  InputSearch,
-  TabsContainer,
-  Tabs,
-  SortButton,
-  Switch,
-  SwitchWithVerticalLabel,
-  Textarea,
-  RadioGroupWithVerticalLabel,
-  DeleteModal,
-  NumberInputWithVerticalLabel,
-  ConfirmModal,
-};
+export * from "./common/Logo";
+export * from "./common/SvgIcon";
+export * from "./interactive/Button";
+export * from "./interactive/Checkbox";
+export * from "./interactive/Input";
+export * from "./interactive/InputPassword";
+export * from "./interactive/InputSearch";
+export * from "./interactive/InputWithVerticalLabel";
+export * from "./interactive/Modal";
+export * from "./interactive/NumberInputWithVerticalLabel";
+export * from "./interactive/RadioGroupWithVerticalLabel";
+export * from "./interactive/Selectbox";
+export * from "./interactive/SortButton";
+export * from "./interactive/Switch";
+export * from "./interactive/SwitchWithVerticalLabel";
+export * from "./interactive/Tabs";
+export * from "./interactive/Textarea";
+export * from "./typography/Text";
