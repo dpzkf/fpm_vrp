8622b74e50748f9580fae1c5289cfbebb925e9f2
Ilya <dpzkfzzz@gmail.com>
2024-06-06 00:58:17 +0300

    fix: page speed performance

--- src/utils/constants/viewState.ts
+++ src/utils/constants/viewState.ts
@@ -3,8 +3,8 @@ import { ViewState } from "react-map-gl";
 const LNG = 35.039749;
 const LAT = 48.432777;
 
-const PITCH = 55.5;
-const ZOOM = 16.38;
+const PITCH = 0;
+const ZOOM = 14;
 
 export const VIEW_STATE: Partial<ViewState> = {
   latitude: LAT,
